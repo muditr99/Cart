377aff6d4c287aad79497a7f0b19f907bc62a215
Mudit Rana <87364334+muditr99@users.noreply.github.com>
2022-03-06 01:58:21 +0530

    added Cart component

---
+++ src/Cart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CartItem from "./CartItem";
+
+class Cart extends React.Component {
+    render () {
+        const arr = [1, 2, 3, 4, 5];
+        return (
+            <div className='cart'>
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                { arr.map((item) => {
+                    return item + 5;
+                }) }
+            </div>
+        )
+    }
+}
+
+export default Cart;
