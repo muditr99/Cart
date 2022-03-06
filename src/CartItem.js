8bbfe3a0af0b5c79b1680b445c459760d0087afb
Mudit Rana <87364334+muditr99@users.noreply.github.com>
2022-03-06 18:01:21 +0530

    moved state from cart to app

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-class CartItem extends React.Component {
+const CartItem = (props) => {
 
-    increaseQuantity () {
+   /* increaseQuantity () {
         // setState form 1
         // this.setState({
         //     qty: this.state.qty + 1,
@@ -31,7 +31,8 @@ class CartItem extends React.Component {
                 qty: prevState.qty - 1,
             }
         })
-    }
+    } 
+*/
 
     // testing () {
     //     const promise = new Promise((resolve, reject) => {
@@ -49,8 +50,7 @@ class CartItem extends React.Component {
     //     })
     // }
 
-    render () {
-        const { price, title, qty } = this.props.product;
+        const { price, title, qty } = props.product;
         return (
             <div className='cart-item'>
                 <div className='left-block'>
@@ -62,14 +62,13 @@ class CartItem extends React.Component {
                     <div style = { { color: '#777' } }> qty: { qty } </div>
                     <div className='cart-item-actions'>
                         {/* buttons */}
-                        <img onClick = {() => this.props.onIncreaseQuantity(this.props.product)} alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
-                        <img onClick = {() => this.props.onDecreaseQuantity(this.props.product)} alt="decrease" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2985/premium/2985073.png?token=exp=1646338483~hmac=fc273f04530eac84880fde701365e114" />
-                        <img onClick = {() => this.props.onDeleteProduct(this.props.product.id)} alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2782/premium/2782988.png?token=exp=1646338698~hmac=97d066ed6ebc72f0d4f5b90c3905f6c0" />                    
+                        <img onClick = {() => props.onIncreaseQuantity(props.product)} alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
+                        <img onClick = {() => props.onDecreaseQuantity(props.product)} alt="decrease" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2985/premium/2985073.png?token=exp=1646338483~hmac=fc273f04530eac84880fde701365e114" />
+                        <img onClick = {() => props.onDeleteProduct(props.product.id)} alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/2782/premium/2782988.png?token=exp=1646338698~hmac=97d066ed6ebc72f0d4f5b90c3905f6c0" />                    
                     </div>
                 </div>
             </div>
         )
-    }
 }
 
 const styles = {
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-class Navbar extends React.Component {
-    render () {
+const Navbar = (props) => {
         return (
             <div style = {styles.nav}>
                 <div style = {styles.cartIconContainer}>
                     <img style = {styles.cartIcon} src='https://cdn-icons-png.flaticon.com/512/891/891462.png' alt='cart-icon'/>
-                    <span style = {styles.cartCount}>3</span>
+                    <span style = {styles.cartCount}>{props.count}</span>
                 </div>
             </div>
         )
-    }
 }
 
 const styles = {
